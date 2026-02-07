67064fad3f46a114f7e77b4d91ed09f3cbdd8d8e
George Ogden <george-ogden@outlook.com>
2026-02-07 15:50:44 +0000

    Add examples with static Nat type checking

---
+++ src/types/nat_types.py
@@ -0,0 +1,27 @@
+from .nat import Nat
+
+
+def succ(x: Nat, /) -> Nat:
+    return x + 1
+
+
+def zero() -> Nat:
+    return 0
+
+
+succ(succ(zero()))
+
+succ(
+    succ(
+        0.0  # type: ignore [arg-type]
+    )
+)
+
+assert isinstance(Nat(0), Nat)
+assert isinstance(0, Nat)
+assert isinstance(1, Nat)
+assert isinstance(-1, Nat)
+
+Nat(5) % Nat(3)
+succ(succ(Nat(3)) // Nat(2))
+"value: " + Nat(5)  # type: ignore [operator]
