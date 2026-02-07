b25e9c43650e4a1be72d05009d2d0b552506000a
George Ogden <george-ogden@outlook.com>
2026-02-07 16:42:29 +0000

    Reject int subclasses as natural number types

--- src/types/nat.py
+++ src/types/nat.py
@@ -9,8 +9,8 @@ else:
     class Nat(DynamicInstantiation, int, metaclass=DynamicCheck):
         @classmethod
         def _is_instance(cls, instance: object) -> bool:
-            return isinstance(instance, int) and instance >= 0
+            return issubclass(type(instance), cls) and instance >= 0
 
         @classmethod
-        def _is_subclass(cls, instance: object) -> bool:
-            return issubclass(instance, int)
+        def _is_subclass(cls, sub_cls: type) -> bool:
+            return sub_cls is int
--- src/types/nat_test.py
+++ src/types/nat_test.py
@@ -1,4 +1,5 @@
 import contextlib
+import enum
 from typing import Any
 
 import pytest
@@ -6,13 +7,29 @@ import pytest
 from . import Nat
 
 
-@pytest.mark.parametrize("typ, expected", [(int, True), (float, False), (bool, True)])
+class Bull(enum.IntEnum):
+    FAWS = 0
+    TWOO = 1
+
+
+@pytest.mark.parametrize(
+    "typ, expected", [(int, True), (float, False), (bool, False), (Bull, False)]
+)
 def test_issubclass(typ: type, expected: bool) -> None:
     assert issubclass(typ, Nat) == expected
 
 
 @pytest.mark.parametrize(
-    "obj, expected", [(1, True), (0, True), (-1, False), (1.5, False), ("1", False)]
+    "obj, expected",
+    [
+        (1, True),
+        (0, True),
+        (-1, False),
+        (1.5, False),
+        ("1", False),
+        (Bull.FAWS, False),
+        (Bull.TWOO, False),
+    ],
 )
 def test_isinstance(obj: object, expected: bool) -> None:
     assert isinstance(obj, Nat) == expected
