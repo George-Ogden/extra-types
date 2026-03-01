627e08952bcacb25cef700cc357503d5656aac10
George Ogden <george-ogden@outlook.com>
2026-03-01 11:26:31 +0000

    Add Python 3.11 support

--- extra_types/types/modifiers.py
+++ extra_types/types/modifiers.py
@@ -1,3 +1,13 @@
-type Modified[T] = T
-type Unmodified[T] = T
-type New[T] = T
+import sys
+from typing import TypeVar
+
+if sys.version_info >= (3, 12):
+    from typing import TypeAliasType
+else:
+    from typing_extensions import TypeAliasType
+
+_T = TypeVar("_T")
+
+Modified = TypeAliasType("Modified", _T, type_params=(_T,))
+Unmodified = TypeAliasType("Unmodified", _T, type_params=(_T,))
+New = TypeAliasType("New", _T, type_params=(_T,))
